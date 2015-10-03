565901c2587308c96277a6fa4193c952c5e7dd58
Brandon Stewart <stewartb2@gmail.com>
2015-10-03 15:17:14 -0400

    Added install.sh to install for mac

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Get and install gcc
+wget https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2 ~/Documnets/
+tar -xvf gcc-arm-none-eabi-4_9-2015q3-20150921-mac.tar.bz2
+echo "export PATH=$PATH:gcc-arm-none-eabi-4_9-2015q3-20150921-mac/bin" > ~/.bashrc
+chmod +x ~/.bashrc
+echo "source ~/.bashrc" > ~/.bash_profile
+
+
+#Get and install stlink
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install stlink
+
+
+
