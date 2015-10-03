b542c766de4d7aee687fbf7beabd8132b30a63c6
Ryan McBee <rmcbee@purdue.edu>
2015-10-03 10:48:25 -0400

    Finialized the starter folder!

---
+++ example.cpp
@@ -0,0 +1,6 @@
+#include "example.h"
+
+void doNothing(void)
+{
+	//this functin does nothing. It is being used as a test
+}
--- starter_folder/main.cpp
+++ starter_folder/main.cpp
@@ -1,18 +1,13 @@
 #include "mbed.h"
 
-Ticker tick;
-DigitalOut led(LED1);
-
-//function to toggle (or switch the state to the other state) the led
-void togglePin(void)  
-{
-        led = !led; // Blink
-} 
+//put non-mbed libraries #defiens here
 
 int main()
 {
-
-    tick.attach_us(togglePin, 100000);  //sets up the ticker to call togglePin every 500000 micro seconds, or half second
-
-    while (1);
+	//initialize your pins and define variables here
+	 
+	while (1)
+	{
+		//put running code here
+	}
 }
