d082b78b356259f981fef1cdbdaf58add66f3141
LMcBee95 <lmcbee@purdue.edu>
2015-11-14 10:55:31 -0500

    adding new files

---
+++ adc_with_dma/inc/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_nucleo.h"
+#include "stm32f4xx_it.h"
+#include "stm32f4xx_hal_conf.h"
+
+/*adc library */
+
+
+#endif /* __MAIN_H */
---
+++ adc_with_dma/src/stm32f4xx_it.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include "stm32f4xx_it.h"
+
+/* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_adc1;
+
+void NMI_Handler(void)
+{
+} 
+
+
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+
+void PendSV_Handler(void)
+{
+}
+
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void DMA2_Stream0_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream0_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream0_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA2_Stream0_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream0_IRQn 1 */
+}
+
+
+static void Error_Handler(void)
+{
+	while(1)
+	{
+	}
+}
